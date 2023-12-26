64dbbd4c788de92491d60f2e651914b1ee716501
Shokhrukh_Developer1 <shokhrukhdeveloper1@gmail.com>
2023-12-26 23:38:48 +0500

    Infra: Upload Project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+System.Console.Write("First number is: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Second number is: ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Adding: {FirstNumber} + {SecondNumber} = {FirstNumber+SecondNumber}");
+System.Console.WriteLine($"Subtracting: {FirstNumber} - {SecondNumber} = {FirstNumber-SecondNumber}");
+System.Console.WriteLine($"Divising: {FirstNumber} / {SecondNumber} = {FirstNumber/SecondNumber}");
+System.Console.WriteLine($"Multiplicating: {FirstNumber} * {SecondNumber} = {FirstNumber*SecondNumber}");
